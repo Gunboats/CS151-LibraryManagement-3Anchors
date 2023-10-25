563c9d03538a123e6e308ca6849672808e2aaf69
Willy <tang3kaw@gmail.com>
2023-10-24 22:41:55 -0700

    Update User.java

--- src/librarymanagement/User.java
+++ src/librarymanagement/User.java
@@ -1,5 +1,37 @@
 package librarymanagement;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
+// 10/24/23
+// We don't have to use an array
+// We may need to limit the amount of objects a user can check out
 
+public class User {
+    private String name;
+    private String cardNumber;
+    private List<Book> borrowedBooks;
+
+    
+    /**
+     * Default constructor
+     * Format for card number should probably indicate it is for a specific
+     * library branch
+     * We don't have to use an ArrayList for borrowed books
+     */
+    User() {
+        this.name = "John Doe";
+        this.cardNumber = "SM-123456789";
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public void checkOut() {
+    	// TODO should add a book for borrowedBooks,
+    	// update borrowedBook to true
+    	
+    }
+    
+    public void checkIn() {
+   
+    }
+    
 }

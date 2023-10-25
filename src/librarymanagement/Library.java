00105f7ee33b6a5302b50da6f6362e01ccc87451
Willy <tang3kaw@gmail.com>
2023-10-25 01:25:44 -0700

    Update Library.java

--- src/librarymanagement/Library.java
+++ src/librarymanagement/Library.java
@@ -1,5 +1,18 @@
 package librarymanagement;
 
-public class Library {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Library {
+    private String name;
+    
+    // ArrayList for sorting books
+    // Map for tracking quantity
+    private List<Book> bookList = new ArrayList<Book>();
+    private Map<Book, Integer> bookListMap = new HashMap<Book, Integer>();
+    private List<User> userList = new ArrayList<User>();
+
+    
 }

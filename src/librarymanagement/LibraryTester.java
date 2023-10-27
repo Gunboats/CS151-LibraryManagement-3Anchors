9e1060d928c65743116d42377629ac300c0fca98
Willy <tang3kaw@gmail.com>
2023-10-26 22:54:38 -0700

    Update LibraryTester.java
    Modified the constructor of library to be updated to reflect changes

--- src/librarymanagement/LibraryTester.java
+++ src/librarymanagement/LibraryTester.java
@@ -2,7 +2,7 @@ package librarymanagement;
 
 public class LibraryTester {
     public static void main(String[] args) {
-        Library library = new Library("Lib");
+        Library library = new Library("San Jose", "SJ");
         library.addBook(new Book("Book", "?", 15761));
         User u = new User("test user");
         library.addUser(u);

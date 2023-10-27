27dc35cd928060f29d8160bc08bfc61383070663
Willy <tang3kaw@gmail.com>
2023-10-26 22:54:07 -0700

    Update LibraryCard.java
    Added getters for LibraryCard, modified the constructor to include the Library's name

--- src/librarymanagement/LibraryCard.java
+++ src/librarymanagement/LibraryCard.java
@@ -3,14 +3,26 @@ package librarymanagement;
 public class LibraryCard {
     private String libraryPrefix;
     private int cardNumber;
+    private String libraryName;
 
-    public LibraryCard(String prefix, int num) {
+    public LibraryCard(String libName, String prefix, int num) {
         libraryPrefix = prefix;
+        libraryName = libName;
         cardNumber = num;
     }
 
+    public String getLibraryPrefix() {
+    	return libraryPrefix;
+    }
+    public int getCardNumber() {
+    	return cardNumber;
+    }
+    public String getLibraryName() {
+        return libraryName;
+    }
+    
     @Override
     public String toString() {
-        return libraryPrefix + "-" + cardNumber;
+        return "Library: " + libraryName + ": " + libraryPrefix + "-" + cardNumber;
     }
 }

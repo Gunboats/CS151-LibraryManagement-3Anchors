3d1d67f924388202d4135d7bce8bdaf2d215d669
pham-t-alex <pham.t.alex@gmail.com>
2023-11-15 00:38:56 -0800

    basic import/export

--- src/librarymanagement/LibraryLoginSignUpFrame.java
+++ src/librarymanagement/LibraryLoginSignUpFrame.java
@@ -5,16 +5,32 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.FileWriter;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.json.*;
+
 public class LibraryLoginSignUpFrame {
-	LibraryLoginSignUpFrame(Library library) {
+	private Library library;
+	
+	LibraryLoginSignUpFrame(Library lib) {
+		library = lib;
 		// Frame for logging in and signing in 
 		JFrame frame = new JFrame("Library Management System");
 		frame.setSize(new Dimension(800, 600));
@@ -62,7 +78,11 @@ public class LibraryLoginSignUpFrame {
 		JButton login = new JButton("  Login ");	
 		login.setAlignmentX(JButton.CENTER_ALIGNMENT);
 		
-		
+		JButton importButton = new JButton("Import");
+		importButton.setAlignmentX(JFileChooser.CENTER_ALIGNMENT);
+
+		JButton exportButton = new JButton("Export");
+		exportButton.setAlignmentX(JFileChooser.CENTER_ALIGNMENT);
 		
 		// Creates a login window when the user presses the button
 		
@@ -101,6 +121,33 @@ public class LibraryLoginSignUpFrame {
 			}
 			
 		});
+
+		importButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showOpenDialog(null);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					setLibrary(file);
+				}
+			}
+		});
+		
+		exportButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int returnVal = fc.showOpenDialog(null);
+				
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					exportLibrary(file);
+				}
+			}
+		});
 		
 		panelNorth.add(Box.createRigidArea(new Dimension(0,150)));
 		
@@ -108,6 +155,10 @@ public class LibraryLoginSignUpFrame {
 		panelCenter.add(login);
 		panelCenter.add(Box.createRigidArea(new Dimension(100,20)));
 		panelCenter.add(signUp);
+		panelCenter.add(Box.createRigidArea(new Dimension(100,20)));
+		panelCenter.add(importButton);
+		panelCenter.add(Box.createRigidArea(new Dimension(100,20)));
+		panelCenter.add(exportButton);
 		panelCenter.add(Box.createVerticalGlue());
 		panelCenter.add(Box.createHorizontalGlue());
 		
@@ -118,4 +169,75 @@ public class LibraryLoginSignUpFrame {
 		frame.add(panelEast, BorderLayout.EAST);
 		frame.setVisible(true);
 	}
+	
+	public void setLibrary(File f) {
+		Library prevLibrary = library;
+		try {
+			JSONObject obj = new JSONObject(new String(Files.readAllBytes(f.toPath())));
+			JSONArray books = obj.getJSONArray("books");
+			JSONArray users = obj.getJSONArray("users");
+			library = new Library(obj.getString("name"), obj.getString("prefix"));
+			for (int i = 0; i < books.length(); i++) {
+				JSONObject bookObj = books.getJSONObject(i);
+				Book b = new Book(bookObj.getString("title"), bookObj.getString("author"));
+				library.addBook(b);
+			}
+			for (int i = 0; i < users.length(); i++) {
+				JSONObject userObj = users.getJSONObject(i);
+				User u = new User(userObj.getString("first_name"), userObj.getString("last_name"), userObj.getString("phone_number"));
+				library.addUser(u);
+			}
+			JOptionPane.showMessageDialog(null, "Library successfully imported.");
+		}
+		catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Invalid File");
+			library = prevLibrary;
+		}
+		catch (JSONException e) {
+			JOptionPane.showMessageDialog(null, "Invalid File");
+			library = prevLibrary;
+		}
+	}
+	
+	public void exportLibrary(File dir) {
+		String path = dir.getPath() + "\\" + library.getName() + ".json";
+		JSONObject libraryObj = new JSONObject();
+		
+		libraryObj.put("name", library.getName());
+		libraryObj.put("prefix", library.getCardPrefix());
+		JSONArray bookArray = new JSONArray();
+		List<Book> bookList = library.getBookList();
+		for (Book b : bookList) {
+			JSONObject bookObj = new JSONObject();
+			bookObj.put("title", b.getBookTitle());
+			bookObj.put("author", b.getAuthor());
+			bookArray.put(bookObj);
+		}
+		libraryObj.put("books", bookArray);
+		
+		JSONArray userArray = new JSONArray();
+		List<User> userList = library.getuserList();
+		for (User u : userList) {
+			JSONObject userObj = new JSONObject();
+			userObj.put("first_name", u.getFirstName());
+			userObj.put("last_name", u.getLastName());
+			userObj.put("phone_number", u.getPhoneNumber());
+			userArray.put(userObj);
+		}
+		libraryObj.put("users", userArray);
+		
+		try {
+			FileWriter writer = new FileWriter(path);
+			writer.write(libraryObj.toString());
+			writer.close();
+			JOptionPane.showMessageDialog(null, "Library successfully exported.");
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Invalid Location");
+		} catch (JSONException e) {
+			JOptionPane.showMessageDialog(null, "Library Error");
+		}
+	}
+}
+
+class InvalidFileException extends Exception {
 }

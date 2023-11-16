4b25af861bfb72e3f198e96022f2340773236a04
laurindaaa <147214348+laurindaaa@users.noreply.github.com>
2023-11-16 14:29:20 -0800

    Update Library.java

--- src/librarymanagement/Library.java
+++ src/librarymanagement/Library.java
@@ -8,24 +8,15 @@ import java.util.Map;
 public class Library {
     private String name;
     private String cardPrefix;
-    // ArrayList for sorting books
-    // Map for tracking quantity
-    private List<Book> bookList = new ArrayList<Book>();
-    private List<User> userList = new ArrayList<User>();  
-    private Map<String, User> cardNumAndUserMap = new HashMap<String, User>();
-    private Map<String, User> phoneNumAndUserMap = new HashMap<String,User>();
-    
-    /**
-     * Default constructor
-     */
+    private List<Book> bookList = new ArrayList<>();
+    private List<User> userList = new ArrayList<>();
+    private Map<String, User> cardNumAndUserMap = new HashMap<>();
+    private Map<String, User> phoneNumAndUserMap = new HashMap<>();
+
     public Library() {
         name = "Library";
     }
 
-    /**
-     * Parameterized constructor
-     * @param name the library's name
-     */
     public Library(String name, String cardPrefix) {
         this.name = name;
         this.cardPrefix = cardPrefix;
@@ -38,34 +29,38 @@ public class Library {
     public void removeBook(Book book) {
         bookList.remove(book);
     }
+
     public boolean containsNumber(String number) {
-    	if(!phoneNumAndUserMap.containsKey(number)) {
-    		return false;
-    	}
-    	return true;
+        return !phoneNumAndUserMap.containsKey(number);
     }
-    
-    public Map<String, User> getPhoneNumAndUserMap()  {
-    	return phoneNumAndUserMap;
+
+    public Map<String, User> getPhoneNumAndUserMap() {
+        return phoneNumAndUserMap;
     }
-    
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
     public void addUser(User user) {
-    	if(!containsNumber(user.getPhoneNumber())) {
+        if (!containsNumber(user.getPhoneNumber())) {
             userList.add(user);
-            
+
             phoneNumAndUserMap.put(user.getPhoneNumber(), user);
-            
+
             user.addCard(new LibraryCard(name, cardPrefix, (int) (Math.random() * 9000000) + 1000000));
             cardNumAndUserMap.put(user.getLibraryCard(cardPrefix).getFullCardID(), user);
-    	}
-
+        }
     }
 
     public void removeUser(User user) {
         userList.remove(user);
         phoneNumAndUserMap.remove(user.getPhoneNumber());
         cardNumAndUserMap.remove(user.getLibraryCard(cardPrefix).getFullCardID());
-        
     }
 
     public void displayBooks() {
@@ -92,18 +87,19 @@ public class Library {
             return null;
         }
         Book book = bookList.remove(index);
-        if (!book.getBorrowed()){
+        if (!book.getBorrowed()) {
             user.borrowBook(book);
             book.setBorrowed(true);
-            System.out.println("User: " + user.getName() + ",borrows book: " + book.getBookTitle() + ", Thank you for your support.");
+            System.out.println("User: " + user.getName() + ", borrows book: " + book.getBookTitle() + ", Thank you for your support.");
         }
         return book;
     }
+
     public String getName() {
         return this.name;
     }
+
     public String getCardPrefix() {
-    	return cardPrefix;
+        return cardPrefix;
     }
-    
 }
